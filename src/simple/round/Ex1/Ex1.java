93722c3f706fa37455399f2bacaaf49af2280005
RAMIL ARSAEV <gattoramm@gmail.com>
2021-03-08 22:12:39 +0300

    add round

---
+++ src/simple/round/Ex1/Ex1.java
@@ -0,0 +1,12 @@
+package simple.round.Ex1;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println(Math.round(3.49));//3
+        System.out.println(Math.round(3.50));//4
+        System.out.println(Math.round(3.51));//4
+        System.out.println(Math.round(-3.49));//-3
+        System.out.println(Math.round(-3.50));//-3
+        System.out.println(Math.round(-3.51));//-4
+    }
+}
