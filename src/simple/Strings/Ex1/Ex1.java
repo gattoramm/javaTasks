114c3256473d244daab99ed8e1daf16abceed922
RAMIL ARSAEV <gattoramm@gmail.com>
2021-03-13 22:48:20 +0300

    noRR

---
+++ src/simple/Strings/Ex1/Ex1.java
@@ -0,0 +1,8 @@
+package simple.Strings.Ex1;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        String f = "Lion.part1.end";
+        System.out.print(f.substring(0, f.indexOf('.')));
+    }
+}
