e4e6ad47e43863466bb85e4f7b353cb90c80050a
RAMIL ARSAEV <gattoramm@gmail.com>
2021-03-30 19:55:11 +0300

    add maven Calculator

---
+++ src/JUNIT/simple1/Calculator/TestCalculator.java
@@ -0,0 +1,13 @@
+package JUNIT.simple1.Calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCalculator {
+    @Test
+    public void testCalc() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(3,4);
+        Assert.assertEquals( 7, result);
+    }
+}
