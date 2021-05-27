b5ce17fd625f79e9e75cd988d01246147de3890e
RAMIL ARSAEV <gattoramm@gmail.com>
2021-05-27 08:51:32 +0300

    Update HexToBin.java

--- src/Numeric/Converter/HexToBin.java
+++ src/Numeric/Converter/HexToBin.java
@@ -5,8 +5,8 @@ import java.math.BigInteger;
 
 public class HexToBin {
     public static void main(String[] args) throws IOException {
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("D:\\hex.txt"));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("D:\\bin.txt"))) {
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("D:\\hex2.txt"));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("D:\\bin2.txt"))) {
             String line;
             while(bufferedReader.ready()) {
                 line = bufferedReader.readLine();
